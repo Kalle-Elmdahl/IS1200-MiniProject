b382a19c67b889ca09756bcdac25173f29db7f8c
erikmalm <70799618+erikmalm@users.noreply.github.com>
2021-02-25 13:39:12 +0100

    Create highscore.c

---
+++ highscore.c
@@ -0,0 +1,5 @@
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+#include "chipsnake.h"  /* Declatations for game */
+#include "i2c-defs.h" /* Declarations of I2C-specific addresses */
+
