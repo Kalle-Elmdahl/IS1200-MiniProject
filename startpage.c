3303fa66cb08b0a9357fcd52d4aec90ccabb0e44
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2021-03-05 10:57:06 +0100

    finalizing.....

--- startpage.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <pic32mx.h>
-#include "mipslab.h"
-
-void draw_start_page() {
-    draw_text(0, 0, "Advanced Snake");
-    draw_text(0, 1, "Make sure all switches are down");
-    draw_text(0, 3, "Press any button to start");
-}
